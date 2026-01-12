948978d3c02a5206b347f0e5488e030bbf2413af
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-13 00:58:59 +0600

    Add serializers and views for retrieving all services and variants; update service views to filter by vendor

--- apps/services/serializers.py
+++ apps/services/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from apps.user.serializers import CustomUserSerializer
 from .models import Service, ServiceVariant
 
 class ServiceVariantSerializer(serializers.ModelSerializer):
@@ -12,3 +14,17 @@ class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = ['id', 'name', 'description', 'variants']
+
+
+class GetAllServiceSerializer(serializers.ModelSerializer):
+    vendor = CustomUserSerializer()
+    class Meta:
+        model = Service
+        fields = ['id','name', 'description', 'variants','vendor']
+
+
+class GetAllServiceVariantSerializer(serializers.ModelSerializer):
+    service = ServiceSerializer()
+    class Meta:
+        model = ServiceVariant
+        fields = ['id', 'service', 'name', 'price', 'estimated_minutes', 'stock']
--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,7 +2,6 @@ from django.http import Http404
 from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
-    TokenVerifyView,
 )
 from .views import (
     SignUpAPIView,
