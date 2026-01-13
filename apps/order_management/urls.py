6fa3e0b3c6ed72d8d0e23ae17b3d6d05d234443b
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-13 11:16:05 +0600

    Add MyOrderListAPIView to retrieve user's orders; update serializers and URLs accordingly

--- apps/order_management/urls.py
+++ apps/order_management/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 
 from .views import (
     OrderCreateAPIView,
-    StripeWebhookAPIView
+    StripeWebhookAPIView,
+    MyOrderListAPIView
 )
 
 urlpatterns = [
     path("create-order/", OrderCreateAPIView.as_view()),
-    # path("my-order-list/", MyOrderListAPIView.as_view()),
+    path("my-order-list/", MyOrderListAPIView.as_view()),
 
 
 
