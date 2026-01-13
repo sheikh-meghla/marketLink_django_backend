698e5fd903963aac39eefaa7075efe80bd260789
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-01-13 08:47:59 +0600

    Implement order management feature: add RepairOrder model, serializers, and views; integrate Stripe for payment processing

---
+++ apps/order_management/models.py
@@ -0,0 +1,30 @@
+import uuid
+from django.db import models
+from apps.services.models import ServiceVariant
+from apps.user.models import CustomUser
+
+
+
+class RepairOrder(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("paid", "Paid"),
+        ("processing", "Processing"),
+        ("completed", "Completed"),
+        ("failed", "Failed"),
+        ("cancelled", "Cancelled"),
+    )
+
+    order_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='customer_orders')
+    vendor = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='vendor_orders')
+    variant = models.ForeignKey(ServiceVariant, on_delete=models.PROTECT)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+
